01f75e6cf3366b37377bd63adab43c6c307594e9
matsukawa_tsubasa <matsukawa_tsubasa@ca-adv.co.jp>
2016-01-18 03:04:18 +0900

    add repository

---
+++ layered-arch-sample/modules/domain/src/main/scala/sample1/domain/lifecycle/user/UserRepository.scala
@@ -0,0 +1,16 @@
+package sample1.domain.lifecycle.user
+
+import sample1.core.util.fujitask.{Task, ReadTransaction}
+import sample1.domain.model.user.{User, UserId}
+import sample1.domain.support.Repository
+
+trait UserRepository extends Repository[UserId, User] {
+
+  /**
+    * エンティティをすべて取得する
+    * @return すべてのエンティティ
+    */
+  def resolveAll: Task[ReadTransaction, List[User]]
+
+}
+
---
+++ layered-arch-sample/modules/domain/src/main/scala/sample1/domain/lifecycle/user/UserRepositoryOnJDBC.scala
@@ -0,0 +1,55 @@
+package sample1.domain.lifecycle.user
+
+import sample1.core.util.fujitask.Task
+import sample1.core.util.fujitask.ReadTransaction
+import sample1.core.util.fujitask.ReadWriteTransaction
+import sample1.infrastructure.scalikejdbc._
+import sample1.domain.model.user.{User, UserId}
+import sample1.infrastructure.scalikejdbc.mysql.models.TUsers
+import scalikejdbc._
+
+object UserRepositoryOnJDBC extends UserRepository {
+
+  /**
+    * エンティティをすべて取得する
+    *
+    * @return すべてのエンティティ
+    */
+  override def resolveAll: Task[Ctx, List[User]] =
+    ask.map { implicit session =>
+      TUsers.findAll().map(UserDxoOnJDBC.toEntity)
+    }
+
+  /**
+    * 識別子に該当するエンティティを取得する
+    *
+    * @param id 識別子
+    * @return 識別子に該当するエンティティ
+    */
+  override def resolveById(id: UserId): Task[ReadTransaction, Option[User]] =
+    ask.map { implicit session =>
+      TUsers.find(id.value).map(UserDxoOnJDBC.toEntity)
+    }
+
+  /**
+    * エンティティを保存する
+    *
+    * @param entity 保存するエンティティ
+    * @return 保存したエンティティ
+    */
+  override def store(entity: User): Task[ReadWriteTransaction, User] =
+    ask.map { implicit session =>
+      UserDxoOnJDBC.toEntity(TUsers.upsert(entity.name, entity.createdAt, entity.updatedAt))
+    }
+
+  /**
+    * 指定した識別子のエンティティを削除する
+    *
+    * @param id 識別子
+    */
+  override def deleteById(id: UserId): Task[ReadWriteTransaction, Unit] =
+    ask.map { implicit session =>
+      TUsers.destroyById(id.value)
+    }
+
+}
